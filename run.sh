7ca15f0a2e3143b5c8c958d7846bbcb979d9b132
Eka Singgih <singgih11@gmail.com>
2019-05-13 02:41:51 +0700

    add ssh echo

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 if [ "${SSH_KEY}" != "null" ]; then
     eval $(ssh-agent -s)
-    echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null    
+    echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+    echo exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
     exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
 else
     echo "SSH_KEY not found"
