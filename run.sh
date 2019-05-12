690ca9ab75f9cecb59eb49295ef66d268d56290f
Eka Singgih <singgih11@gmail.com>
2019-05-13 01:54:01 +0700

    fix shh tunneling script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "${SSH_KEY}" != "null" ]; then
-    ssh-add < echo ${SSH_KEY}
-
+    eval $(ssh-agent -s)
+    echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null    
     exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
 else
     echo "SSH_KEY not found"
