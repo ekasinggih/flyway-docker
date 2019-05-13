219bc815d70da50d4d9317fdcb28fa92699b8352
Eka Singgih <singgih11@gmail.com>
2019-05-13 11:57:57 +0700

    change to non interactive mode

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [ "${SSH_KEY}" != "null" ]; then
     eval $(ssh-agent -s)
     echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null    
-    exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT
+    exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
 else
     echo "SSH_KEY not found"
     exit 1
