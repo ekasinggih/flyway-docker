bdcafd3bd7efae9617905aeb754947267ffd79a6
Eka Singgih <singgih11@gmail.com>
2019-05-13 11:41:38 +0700

    update tunnel script

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@
 
 if [ "${SSH_KEY}" != "null" ]; then
     eval $(ssh-agent -s)
-    echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
-    echo exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
-    exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT -N
+    echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null    
+    exec ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -L $LOCAL_PORT:$REMOTE_IP:$REMOTE_PORT
 else
     echo "SSH_KEY not found"
     exit 1
